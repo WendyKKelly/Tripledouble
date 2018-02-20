26bb3a0f17292837c90ccef0a13becc1be8d59bf
WendyKKelly <wendykkelly@gmail.com>
2018-02-20 10:30:21 -0800

    first major change

--- Tripledouble.js
+++ Tripledouble.js
@@ -1,18 +1,45 @@
+/* function tripledouble(num1, num2) {  */
+var num1 = 1222345;
+/* var num2 = 12345;  */
+/* if ( */
+var re = new RegExp('^\d*(\d)\1{2}\d*$', 'g');
+numSplit = num1.test(re);
+alert(numSplit);
+/* === false) {
+alert(0);
+} */ /* else {
+const numSplit = num1.toString().match(/^\d*(\d)\1{2}\d*$/g).toString();
+alert(typeof numSplit);
+} */
+/* if (numSplit !== null) {
+alert(numSplit);
 
-function tripledouble(num1, num2) {
-console.log(num1);
-console.log(num2);
-const numSplit = num1.toString().split(/^\d*(\d)\1{2}\d*$/g).toString();
+} */
+/*
 const numTrim = numSplit.replace(/[, ]+/g, " ").trim();
-const numVar = new RegExp('^\\d*(' + numTrim + ')\\1{1}\\d*$');
-num2Str = num2.toString();
+alert(numTrim);
+var index = numTrim.indexOf(" ");  // Gets the first index where a space occours
+var id = numTrim.substr(0, index);
+alert(id.charAt(0));// Gets the first part
+var text = numTrim.substr(index + 1);
+alert(text.charAt(0));
+alert(id);
+alert(text);
+var numTrim1 = id.charAt(0);
+alert(numTrim1);
+var numTrim2 = text.charAt(0);
+const numVar = new RegExp('(' + numTrim1 + '|' + numTrim2 + '){1}', 'g');
+alert(numVar);
+var num2Str = num2.toString();
 const num2Split = numVar.test(num2Str);
 if (num2Split === true) {
 return 1;
 } else {
 return 0;
 }
+
 }
-var num1 = 7990;
-var num2 = 999999999999;
-tripledouble(num1, num2);
+*/
+/* var num1 = 12345;
+var num2 = 12345;
+alert(tripledouble(num1, num2));  */
